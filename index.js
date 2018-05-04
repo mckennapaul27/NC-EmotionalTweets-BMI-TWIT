bd8e9e01276473f287eaf09777932273fc6efbf0
Paul McKenna <mckennapaul27@gmail.com>
2018-05-04 15:12:18 +0100

    Correctly linked up to API's and quick test

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-'This is the index file'
+const util = require('util');
+const {T} = require('./lib/twitter');
+const {languageTranslator} = require('./lib/watson');
+
+function translateToSpanish (text) {
+    languageTranslator.translate({
+        text,
+        source: 'en',
+        target: 'es'
+    })
+    .then(data => { 
+               
+        return data.translations;
+    })
+    .then(result => {
+        result.forEach(phrase => {            
+            T.post('statuses/update', {status: phrase.translation}, function(err, data, response) {
+                console.log(`${phrase.translation} posted successfully`)
+              });
+        });      
+    })
+    .catch(err => {
+        throw err;
+    })
+}
+
+translateToSpanish(['hi, how are you?', 'my name is paul'])
